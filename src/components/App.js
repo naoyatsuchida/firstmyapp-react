ec8005dfa9078f6f62098f1e690db6de867352ce
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-03-22 16:45:33 +0900

    完成

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React ,{useState,useEffect}from 'react';
+import AppContext from '../context/AppContext'
+import '../App.scss';
+import Head from './top/Header'
+import Main from './top/Main'
 import axios from 'axios'
 
 function App() {
+
+  const [article, setArticle] = useState([]);
+
+  useEffect(()=>{
+    news();
+  },[])
+
+  const news = async() =>{
+    const url = 'http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=e50c1e192d364a2a8f35150d178f8df8'
+    try {
+      const response = await axios.get(url);
+      setArticle(response.data.articles)
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  console.log(article)
+
+
+
+
   return (
-    <React.Fragment>
+    <AppContext.Provider value={{article}}>
+
       <div className='wrapper'>
-
-
+    
+        <Head/>
+        <Main/>
+      
+        <p　className="api">外部apiの索引を春</p>
       </div>
-    </React.Fragment>
+    
+    </AppContext.Provider>
   );
 }
 
---
+++ src/components/top/Main.js
@@ -0,0 +1,42 @@
+import React ,{useContext} from 'react';
+import Kizi from './Kizi'
+import AppContext from "../../context/AppContext"
+
+const Main =()=>{
+  const {article} = useContext(AppContext)
+  
+  const item = article.map((art,index) =>{
+    return(
+      <Kizi
+      title={art.title}
+      image={art.urlToImage}
+      body ={art.description}
+      key={index}
+      
+      />
+    )
+  })
+
+
+
+  return(
+    <div className="main">
+          {/* サイドーーーー   railsからお気に入り記事データの所得をする*/}
+          <div className="said">
+            <div className="said__box">
+              <img className="said-image" src="https://picsum.photos/id/237/200/300"></img>
+              <div className="said-content">直也は初めてアプリを作った</div>
+            </div>
+          </div>
+{/* 　　　　　　　　　メインコンテンツーーーー */}
+          <div className="content">
+            
+              {item}
+
+          </div>
+        </div>
+
+  )
+}
+
+export default Main
