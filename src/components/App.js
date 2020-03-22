de7de045071a85c3b9426b40cebb42c7640cebb0
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-03-22 12:48:26 +0900

    f

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import axios from 'axios'
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className='wrapper'>
+
+
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+
